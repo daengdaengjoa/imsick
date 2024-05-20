050e97fc4fc78dbf3a0868132dd2483b46fd0edb
uparupa95 <qupa300@gmail.com>
2024-05-20 11:15:51 +0900

    상세조회 추가

--- imsick/articles/serializers.py
+++ imsick/articles/serializers.py
@@ -4,4 +4,4 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ["pk", "title", "content", "created_at", "updated_at"]
+        fields = ["article_id", "title", "content", "created_at", "updated_at"]
--- imsick/articles/views.py
+++ imsick/articles/views.py
@@ -1,26 +1,44 @@
-from rest_framework import APIView
+from rest_framework.views import APIView
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
 from .models import Article
 from .serializers import ArticleSerializer
 
-from django.shortcuts import render
-
-
-# Create your views here.
 
 class HospitalAPIView(APIView):
-    #게시물 전체 조회
+    # 게시물 전체 조회
     def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    #게시물 생성
+
+    # 게시물 생성
     def post(self, request):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP.201_CREATED)
         return Response(serializer.errors, status=status.HTTP.400_BAD_REQUEST)
+    
+class HospitalDetailAPIView(APIView):
+    #게시물 상세조회
+    def get(self,request,pk):
+        article = get_object_or_404(Article, article_id=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    #게시물 수정
+    def put(self,request, pk):
+        article = get_object_or_404(Article, article_id=pk)
+        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP.400_BAD_REQUEST)
+
+    #게시물 삭제
+    def delete(self, request, pk):
+        article = get_object_or_404(Article, article_id=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NOT_CONTENT)
